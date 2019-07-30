99c4cda3dec3f778edd2a7623354ca7ee00c054d
Viktor geozukunft <geozukunft@gmail.com>
2019-07-30 21:26:01 +0200

    inital code

--- forwardUserbot/forwardUserbot.py
+++ forwardUserbot/forwardUserbot.py
@@ -1,1 +1,20 @@
+import asyncio
+import datetime
+import os
+import time
 
+import schedule
+import telethon
+import telethon.sync
+
+
+
+
+
+
+with telethon.sync.TelegramClient('geosession', api_id, api_hash, lang_code='de') as client:
+    me = client.get_me()
+
+    username = me.username
+    print(username)
+    print(me.phone)
